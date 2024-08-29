141cd3893c7ab6ca8014dc07463da1c373f1d8e0
IldenH <IldenH.1@proton.me>
2024-08-29 10:00:48 +0200

    Dårlig løsing stein saks papir oppgave

---
+++ intro-python/stein_saks_papir.py
@@ -0,0 +1,51 @@
+import random
+
+mulige_trekk: list[str] = ["stein", "saks", "papir"]
+
+
+def spiller_vant(trekk: str, datamaskin_trekk: str) -> bool:
+    return (
+        (trekk == "stein" and datamaskin_trekk == "saks")
+        or (trekk == "saks" and datamaskin_trekk == "papir")
+        or (trekk == "papir" and datamaskin_trekk == "stein")
+    )
+
+
+def print_mulige_trekk():
+    print("Velg en av:")
+    for trekk in mulige_trekk:
+        print(trekk, end=",\n")
+
+
+poeng = 0
+poeng_datamaskin = 0
+
+while poeng < 3 or poeng_datamaskin < 3:
+    datamaskin_trekk: str = mulige_trekk[random.randint(0, 2)]
+
+    while True:
+        print_mulige_trekk()
+        trekk: str = input("\n").lower()
+
+        try:
+            mulige_trekk.index(trekk)
+        except:
+            continue
+        else:
+            break
+
+    print(f"\nDu valgte: {trekk}, jeg valgte: {datamaskin_trekk}\n")
+
+    if trekk == datamaskin_trekk:
+        print("Uavgjort, ingen fikk poeng.")
+    elif spiller_vant(trekk, datamaskin_trekk):
+        print("Du fikk et poeng!")
+        poeng += 1
+    else:
+        print("Du tapte og jeg fikk et poeng.")
+        poeng_datamaskin += 1
+
+if poeng > poeng_datamaskin:
+    print("Du vant!")
+else:
+    print("Du tapte og jeg vant.")
