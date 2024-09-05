c0cbe4c9bd7aa00eb6f72c2816e5f7a638586f59
IldenH <IldenH.1@proton.me>
2024-09-05 09:27:35 +0200

    Forekomster av bokstaver i wikipedia side om norge

---
+++ større-oppgaver/forekomster_av_bokstaver.py
@@ -0,0 +1,39 @@
+tekst = open("wikipedia_norge.txt", "r").read()
+
+bokstaver = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    "æ": 0,
+    "ø": 0,
+    "å": 0,
+}
+
+for bokstav in tekst:
+    if bokstav in bokstaver:
+        bokstaver[bokstav] += 1
+
+print(bokstaver)
