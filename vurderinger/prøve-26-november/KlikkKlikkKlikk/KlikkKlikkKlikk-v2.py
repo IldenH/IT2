c0780e2ed618be2de552bf9ac431d8d3921c37c5
IldenH <IldenH.1@proton.me>
2024-11-26 14:29:51 +0100

    Heldagsprøve: fix KlikkKlikkKlikk-v2

--- vurderinger/prøve-26-november/KlikkKlikkKlikk/KlikkKlikkKlikk-v2.py
+++ vurderinger/prøve-26-november/KlikkKlikkKlikk/KlikkKlikkKlikk-v2.py
@@ -14,7 +14,7 @@ TIME_SECONDS = 2
 
 
 class Game:
-    def __init__(self):
+    def __init__(self) -> None:
         self.running = True
         self.screen = pygame.display.set_mode([SCREEN.width, SCREEN.height])
         self.fps = 60
@@ -24,47 +24,58 @@ class Game:
             random.randint(0, 255),
             random.randint(0, 255),
         )
+        self.game_end_event = pygame.USEREVENT
+        self.beste_resultat = 0
 
-    def run(self):
+    def run(self) -> None:
         pygame.init()
-        pygame.time.set_timer(pygame.QUIT, TIME_SECONDS * 1000)
+        pygame.time.set_timer(self.game_end_event, TIME_SECONDS * 1000)
         while self.running:
             self.handle_events()
             self.update()
         pygame.quit()
 
-    def update(self):
+    def update(self) -> None:
         self.draw()
 
         pygame.display.update()
         pygame.time.Clock().tick(self.fps)
 
-    def draw(self):
+    def draw(self) -> None:
         self.screen.fill(self.background_color)
 
-    def handle_events(self):
+    def handle_events(self) -> None:
         for event in pygame.event.get():
             escape = event.type == key.KEYDOWN and event.key == key.K_ESCAPE
             if event.type == pygame.QUIT or escape:
                 self.running = False
+            elif event.type == self.game_end_event:
+                self.handle_new_game()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 self.clicks += 1
 
+    def handle_new_game(self) -> None:
+        print(f"Du trykket {self.clicks} ganger!")
+
+        if self.clicks > self.beste_resultat:
+            print("Ny rekord!")
+            self.beste_resultat = self.clicks
+        else:
+            print("Ikke ny rekord, prøv igjen!")
+
+        self.background_color = (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        )
+
 
 if __name__ == "__main__":
-    beste_resultat = 0
     print(f"Trykk så fort som mulig på {TIME_SECONDS} sekunder!")
     game = Game()
     game.run()
-    print(f"Du trykket {game.clicks} ganger!")
 
-    if game.clicks > beste_resultat:
-        print("Ny rekord!")
-        beste_resultat = game.clicks
-    else:
-        print("Ikke ny rekord, prøv igjen!")
-
-    resultat_tekst = f"Beste resultat: {beste_resultat}"
+    resultat_tekst = f"Beste resultat: {game.beste_resultat}"
     print(resultat_tekst)
     # https://www.geeksforgeeks.org/writing-to-file-in-python/
     resultat_fil = open("resultat.txt", "w")
