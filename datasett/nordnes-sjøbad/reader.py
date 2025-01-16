f79befbbda5ed99207756af98a08aa7784a024eb
IldenH <IldenH.1@proton.me>
2025-01-16 09:59:11 +0100

    nordnes: cleanup code

--- datasett/nordnes-sjøbad/reader.py
+++ datasett/nordnes-sjøbad/reader.py
@@ -2,30 +2,41 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-file = "./data.csv"
 
-data = {}
+def parse_row(row):
+    tid = int(datetime.strptime(row["tid"], "%Y-%m-%d %H:%M:%S").timestamp())
 
-with open(file, encoding="utf-8") as fil:
-    filinnhold = csv.DictReader(fil, delimiter=",")
+    try:
+        temperatur = float(row["temperatur"])
+    except ValueError:
+        temperatur = "-"
 
-    for rad in filinnhold:
-        tid = int(datetime.strptime(rad["tid"], "%Y-%m-%d %H:%M:%S").timestamp())
-        try:
-            temperatur = float(rad["temperatur"])
-        except ValueError:
-            temperatur = "-"
-
-        if temperatur != "-":
-            data[tid] = temperatur
+    if temperatur != "-":
+        data[tid] = temperatur
 
 
-tid = list(data.keys())
-temperatur = list(data.values())
+def parse_file(contents: csv.DictReader):
+    for row in contents:
+        parse_row(row)
 
-plt.plot(tid, temperatur)
-plt.xlabel("Tid")
-plt.ylabel("Temperatur")
-plt.title("Nordnes sjøbad temperatur i vannet over tid")
-plt.grid()
-plt.show()
+
+def plot(data):
+    tid = list(data.keys())
+    temperatur = list(data.values())
+
+    plt.plot(tid, temperatur)
+    plt.xlabel("Tid")
+    plt.ylabel("Temperatur")
+    plt.title("Nordnes sjøbad temperatur i vannet over tid")
+    plt.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    file = "./data.csv"
+    data = {}
+
+    with open(file, encoding="utf-8") as file:
+        parse_file(csv.DictReader(file, delimiter=","))
+
+    plot(data)
