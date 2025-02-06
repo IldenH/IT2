98239d85b890dbff2d6c7a9c5f4e915b65548033
IldenH <IldenH.1@proton.me>
2025-02-06 08:56:21 +0100

    wca: persons pagination

--- datasett/wca/persons.py
+++ datasett/wca/persons.py
@@ -1,9 +1,30 @@
 import pandas as pd
+import requests
 
-api = "https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/"
-url = api + "persons.json"
+api = "https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api"
+url = f"{api}/persons.json"
 
-df = pd.read_json(url, typ="series")
-df = pd.DataFrame(df["items"])
+params = {
+    "page": 0,
+    "size": 1000,
+}
 
-print(df)
+all_data = []
+while True:
+    response = requests.get(url, params)
+    if response.status_code != 200:
+        break
+
+    data = response.json()
+    if not data:
+        break
+    all_data.extend(data)
+
+    params["page"] += 1
+
+
+print(all_data)
+# df = pd.read_json(all_data, typ="series")
+# df = pd.DataFrame(df["items"])
+#
+# print(df)
